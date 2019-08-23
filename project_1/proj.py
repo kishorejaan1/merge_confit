a7c83f03c426c7d7cda86da0c5277f3b69d1e1dc
Venkata Kishore <kishoremca.0053@gmail.com>
2019-08-23 20:01:47 +0530

    Commiting proj.py for merge conflict check

--- project_1/proj.py
+++ project_1/proj.py
@@ -1,6 +1,6 @@
-
-
-Python is a scripting and programming language.. !!!
-
-
-New content adding... 
+1
+2
+3Python is a scripting and programming language.. !!!
+4
+5
+6 Hey Conflict how are u !! New content adding... 
