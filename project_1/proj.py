0a14cb3409e27324ccdf4383396fd982debb2c4e
Venkata Kishore <kishoremca.0053@gmail.com>
2019-08-23 19:59:02 +0530

    Modifying file proj.py from create_branch

--- project_1/proj.py
+++ project_1/proj.py
@@ -1,6 +1,10 @@
 
 
-Python is a scripting and programming language.. !!!
-
-
-New content adding... 
+1 Python is a scripting and programming language.. !!!
+2
+3
+4 New content adding... 
+5
+6 Hello Merge Conflict
+7
+8
