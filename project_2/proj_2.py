b86f3128337e5f9ec725577ef9203364ac6afd0a
Venkata Kishore <kishoremca.0053@gmail.com>
2019-08-24 13:07:26 +0530

    Commiting from B user

--- project_2/proj_2.py
+++ project_2/proj_2.py
@@ -1,13 +1,10 @@
 
-
 Hello Buddy this is pypy program.
-
-
 Adding something for second time...
-
-
 Testing to push data to**
 Testing to push data to branch ....
-
 REmove
 Adding to existing one only ...
+
+
+Hey Am B user... 
